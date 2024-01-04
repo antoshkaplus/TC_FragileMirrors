c5b58d59c6d88dab6b3b2b772388969b2f43fa3d
Anton Logunov <antoshkaplus@gmail.com>
2024-01-03 20:35:51 -0800

    go

---
+++ src/mirrors/board/board_r.cpp
@@ -0,0 +1,27 @@
+#include "mirrors/board/board_r1.hpp"
+#include "mirrors/board/board_r2.hpp"
+#include "mirrors/board/board_r3.hpp"
+#include "mirrors/board/board_r4.hpp"
+#include "mirrors/board/board_r5.hpp"
+#include "mirrors/board/board_r6.hpp"
+
+
+std::vector<mirrors::cell_count_t> mirrors::Board_r::restore;
+std::vector<mirrors::cell_count_t> mirrors::Board_r2::restore;
+
+std::vector<mirrors::cell_count_t> mirrors::Board_r3::restore;
+mirrors::board_size_t mirrors::Board_r3::size_;
+
+uint32_t mirrors::Board_r4::destroyed_idx = 0;
+mirrors::Grid<uint32_t> mirrors::Board_r4::destroyed;
+std::vector<mirrors::Position> mirrors::Board_r4::destroyed_pos;
+
+uint32_t mirrors::Board_r5::destroyed_idx = 0;
+mirrors::Grid<uint32_t> mirrors::Board_r5::destroyed;
+std::vector<mirrors::Position> mirrors::Board_r5::destroyed_pos;
+std::vector<mirrors::cell_count_t> mirrors::Board_r5::destroyed_cast;
+
+uint32_t mirrors::Board_r6::destroyed_idx = 0;
+mirrors::Grid<uint32_t> mirrors::Board_r6::destroyed;
+std::vector<mirrors::Position> mirrors::Board_r6::destroyed_pos;
+std::vector<mirrors::cell_count_t> mirrors::Board_r6::destroyed_cast;
